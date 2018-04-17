de2c78652d3784fb9ee4004fe8a474a64774db9d
Richard Roots <rickyroots@yahoo.com>
2018-04-16 22:44:33 -0700

    Added to Rickys Scene, updates to AI behavior, ref #3

---
+++ Assets/Scripts/Enemy/ZomBear.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZomBear : MonoBehaviour {
+
+	public GameObject player;
+	public AudioClip[] footsounds;
+	private UnityEngine.AI.NavMeshAgent nav;
+	private AudioSource sound;
+	private Animator anim;
+	private string state = "idle";
+	private bool alive = true;
+
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+		sound = GetComponent<AudioSource> ();
+		anim = GetComponent<Animator> ();
+		nav.speed = 1.2f;
+		anim.speed = 1.2f;
+	}
+
+	public void footstep(int _num)
+	{
+		sound.clip = footsounds [_num];
+		sound.Play ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (alive == true) {
+			
+			anim.SetFloat ("velocity", nav.velocity.magnitude);
+			//nav.SetDestination (player.transform.position);
+
+			if (state == "idle") {
+				//pick random place to walk
+				Vector3 randomPos = Random.insideUnitSphere*20f;
+				NavMeshHit navHit;
+				NavMesh.SamplePosition (transform.position + randomPos, out navHit, 20f, NavMesh.AllAreas);
+				nav.SetDestination (navHit.position);
+				state = "walk";
+			}
+
+			if (state == "walk") {
+				if(nav.remainingDistance <= nav.stoppingDistance && !nav.pathPending){
+					state = "idle";
+				}
+			}
+
+		}
+	}
+}
