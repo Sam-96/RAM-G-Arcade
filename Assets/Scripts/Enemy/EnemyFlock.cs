77210c0b6d3f8b89d9de71d223a16f5ad66846fe
Richard Roots <rickyroots@yahoo.com>
2018-04-16 17:31:04 -0700

    Added enemy flocking scripts, ref #3

---
+++ Assets/Scripts/Enemy/EnemyFlock.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour {
+
+	public float movementSpeed = 0.8f;
+	float turnSpeed = 5.0f;
+	Vector3 averageHeading;
+	Vector3 averagePosition;
+	float neighborDistance = 10.0f;
+	bool turning = false;
+
+	// Use this for initialization
+	void Start () {
+		movementSpeed = Random.Range (0.5f, 0.8f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (transform.position, Vector3.zero) >= LazyFlight.roamSize) {
+			turning = true;
+		} 
+		else {
+			turning = false;
+		}
+
+		if (turning == true) {
+			Vector3 direction = Vector3.zero - transform.position;
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), turnSpeed * Time.deltaTime);
+
+			movementSpeed = Random.Range (0.5f, 0.8f);
+		} 
+		else {
+			if (Random.Range (0, 5) < 1)
+				ApplyRules ();
+		}
+		transform.Translate(0,0, Time.deltaTime *movementSpeed);
+	}
+
+	void ApplyRules()
+	{
+		GameObject[] gos;
+		gos = LazyFlight.allBoidz;
+
+		Vector3 vcenter = Vector3.zero;
+		Vector3 vavoid = Vector3.zero;
+		float gSpeed = 0.1f;
+
+		Vector3 goalPos = LazyFlight.goalPos;
+
+		float dist;
+		int groupSize = 0;
+
+		foreach (GameObject go in gos) {
+			if (go != this.gameObject) {
+				dist = Vector3.Distance (go.transform.position, this.transform.position);
+				if (dist <= neighborDistance) {
+					vcenter += go.transform.position;
+					groupSize++;
+
+					if (dist < 1.0f)
+					{
+						vavoid = vavoid + (this.transform.position - go.transform.position);
+					}
+
+					Flock anotherFlock = go.GetComponent<Flock>();
+					gSpeed = gSpeed + anotherFlock.movementSpeed;
+				}
+			}
+		}
+
+		if(groupSize > 0)
+		{
+			vcenter = vcenter/groupSize + (goalPos - this.transform.position);
+			movementSpeed = gSpeed/groupSize;
+
+			Vector3 direction = (vcenter + vavoid) - transform.position;
+			if(direction != Vector3.zero)
+				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+		}
+	}
+}
--- Assets/Scripts/Enemy/EnemyFlockingBehavior.cs
+++ Assets/Scripts/Enemy/EnemyFlockingBehavior.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFlockingBehavior : MonoBehaviour {
+public class LazyFlight : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject boidPrefab;
+	public GameObject goalPrefab;
+	public static int roamSize = 20;
+	public static int targetRange = 8;
+	public bool followPlayer = true;
+
+	static int numBoidz = 10;
+	public static GameObject[] allBoidz = new GameObject[numBoidz];
+	public static Vector3 goalPos = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-		
+		for (int i = 0; i < numBoidz; i++) {
+			Vector3 pos = new Vector3(	Random.Range (-targetRange, targetRange),
+				Random.Range (0,1),
+				Random.Range (-targetRange, targetRange));
+			allBoidz [i] = (GameObject)Instantiate (boidPrefab, pos, Quaternion.identity);
+
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (followPlayer == true) {
+			goalPos = player.transform.position;
+
+			//var playerObject = GameObject.Find("Player");
+			//goalPos = playerObject.transform.position;
+		}
+
 	}
 }
