057fee15ab5ecebffd11685be4b2b448d6657ec1
Richard Roots <rickyroots@yahoo.com>
2018-05-03 12:07:48 -0700

    Making multiplayer lobby for Rickys Scene, ref #03

---
+++ test_project/assets/Ricky/scripts/RickyLocalPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class RickyLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GetComponent<RigidbodyFirstPersonController> ().enabled = true;
+			GetComponentInChildren<Camera> ().enabled = true;
+			GetComponent<MouseLook> ().XSensitivity = 2;
+			GetComponent<MouseLook> ().YSensitivity = 2;
+		}
+		else{
+			GetComponent<RigidbodyFirstPersonController> ().enabled = false;
+			GetComponentInChildren<Camera> ().enabled = false;
+			GetComponent<MouseLook> ().XSensitivity = 0;
+			GetComponent<MouseLook> ().YSensitivity = 0;
+		}
+
+		this.transform.position = new Vector3 (Random.Range (-20, 20), 0, Random.Range (-20, 20));
+	}
+
+
+}
